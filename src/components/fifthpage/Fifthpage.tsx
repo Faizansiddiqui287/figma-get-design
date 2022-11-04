29187f5a05f6984f136b1f8641fb9f3b9823953e
Faizansiddiqui287 <faizansiddiqui287@gmail.com>
2022-11-04 12:18:36 +0530

    created file structure

---
+++ src/components/fifthpage/Fifthpage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fifthpage = () => {
+  return (
+    <div>Fifthpage</div>
+  )
+}
+
+export default Fifthpage
---
+++ src/components/fourthpage/Fourthpage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fourthpage = () => {
+  return (
+    <div>Fourthpage</div>
+  )
+}
+
+export default Fourthpage
